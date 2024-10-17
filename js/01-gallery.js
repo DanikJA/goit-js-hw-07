c1d01c667c53b3474fd79f7397927bead46e23e6
Danik2023 <danik.kyrteu2@gmail.com>
2024-10-17 20:29:33 +0300

    created img list

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,4 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
- kbjbhjbhb
+  
+
+
+
+
+const galleryList = document.querySelector('.gallery');
+const getGel = createImgCards(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', getGel )
+
+function createImgCards(cards){
+    const murkUp = cards.map(card => {
+        return `<li class="gallery__item">
+                 <a class="gallery__link" href="${card.original}">
+                <img class="gallery__image" src="${card.preview}" data-source="${card.original}" alt="${card.description}" />
+                 </a>
+                </li>`
+        
+    }).join('');
+    return murkUp;
+}
+
+
+
+
+
+
+
