bae9199b201a0b8e03760f78c8010a763ce55104
Danik2023 <danik.kyrteu2@gmail.com>
2024-10-18 18:13:29 +0300

    .

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,55 +2,44 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+  
 
 const galleryEl = document.querySelector('.gallery');
 
 const getEl = makeGalleryFn(galleryItems);
 galleryEl.insertAdjacentHTML('beforeend', getEl);
 
-function makeGalleryFn(cards) {
+function makeGalleryFn(cards){
     const markUp = cards.map(card => {
-        return <li class="gallery__item">
-            <a class="gallery__link" href="${card.original}">
-                <img
-                    class="gallery__image"
-                    src="${card.preview}"
-                    data-source="${card.original}"
-                    alt="${card.description}"
-                />
-            </a>
-        </li>;
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="${card.original}">
+    <img
+      class="gallery__image"
+      src="${card.preview}"
+      data-source="${card.original}"
+      alt="${card.description}"
+    />
+  </a>
+</li>`
     }).join('');
     return markUp;
 }
-
-let instance;
 
 function openModalPhotoOnClick(event) {
     event.preventDefault();
 
     const currentImg = event.target.dataset.source;
     
-    if (!currentImg) {
+    if(!currentImg) {
         return;
     }
     
-    instance = basicLightbox.create(
-        <img src="${currentImg}" width="800" height="600">
-    , {
-        onClose: () => {
-            window.removeEventListener('keydown', closeOnClick);
-        }
-    });
+  const instance = basicLightbox.create(`
+    <img src="${currentImg}" width="800" height="600">
+`);
 
-    instance.show();
-    window.addEventListener('keydown', closeOnClick);
+
+instance.show()
 }
 
-function closeOnClick(event) {
-    if (event.code === 'Escape') {
-        instance.close();
-    }
-}
-
-galleryEl.addEventListener('click', openModalPhotoOnClick);
+galleryEl.addEventListener('click', openModalPhotoOnClick);   
