6311821c5af8135ba5b3b7f73e47af4c2a6bbebc
Danik2023 <danik.kyrteu2@gmail.com>
2024-10-18 13:45:01 +0300

    added new function

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,29 +4,58 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
   
 
+// const galleryList = document.querySelector('.gallery');
+// const getGel = createImgCards(galleryItems);
 
+// galleryList.insertAdjacentHTML('beforeend', getGel )
 
-
-const galleryList = document.querySelector('.gallery');
-const getGel = createImgCards(galleryItems);
-
-galleryList.insertAdjacentHTML('beforeend', getGel )
-
-function createImgCards(cards){
-    const murkUp = cards.map(card => {
-        return `<li class="gallery__item">
-                 <a class="gallery__link" href="${card.original}">
-                <img class="gallery__image" src="${card.preview}" data-source="${card.original}" alt="${card.description}" />
-                 </a>
-                </li>`
+// function createImgCards(cards){
+//     const murkUp = cards.map(card => {
+//         return `<li class="gallery__item">
+//                  <a class="gallery__link" href="${card.original}">
+//                 <img class="gallery__image" src="${card.preview}" data-source="${card.original}" alt="${card.description}" />
+//                  </a>
+//                 </li>`
         
+//     }).join('');
+//     return murkUp;
+// }
+
+
+
+const galleryEl = document.querySelector('.gallery');
+
+const getEl = makeGalleryFn(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', getEl);
+
+function makeGalleryFn(cards){
+    const markUp = cards.map(card => {
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="${card.original}">
+    <img
+      class="gallery__image"
+      src="${card.preview}"
+      data-source="{card.original}"
+      alt="${card.description}"
+    />
+  </a>
+</li>`
     }).join('');
-    return murkUp;
+    return markUp;
 }
 
 
+galleryEl.addEventListener('click', openModalPhotoOnClick);
 
+function openModalPhotoOnClick(event) {
+    event.preventDefault();
 
+    const currentImg = event.target.dataset.source;
 
+    const instance = basicLightbox.create(`
+    <img src="${currentImg}" width="800" height="600">
+`)
 
+instance.show()
+}
 
