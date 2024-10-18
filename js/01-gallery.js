2a5dae9d3a6c49c0baca8159ae3f87c0a11a5d1a
Danik2023 <danik.kyrteu2@gmail.com>
2024-10-18 14:31:07 +0300

    changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,66 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-  
+const galleryContainerEl = document.querySelector(".gallery");
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryContainerEl.insertAdjacentHTML("beforeend", imagesMarkup);
 
-// const galleryList = document.querySelector('.gallery');
-// const getGel = createImgCards(galleryItems);
-
-// galleryList.insertAdjacentHTML('beforeend', getGel )
-
-// function createImgCards(cards){
-//     const murkUp = cards.map(card => {
-//         return `<li class="gallery__item">
-//                  <a class="gallery__link" href="${card.original}">
-//                 <img class="gallery__image" src="${card.preview}" data-source="${card.original}" alt="${card.description}" />
-//                  </a>
-//                 </li>`
-        
-//     }).join('');
-//     return murkUp;
-// }
-
-
-
-const galleryEl = document.querySelector('.gallery');
-
-const getEl = makeGalleryFn(galleryItems);
-galleryEl.insertAdjacentHTML('beforeend', getEl);
-
-function makeGalleryFn(cards){
-    const markUp = cards.map(card => {
-        return `<li class="gallery__item">
-  <a class="gallery__link" href="${card.original}">
-    <img
-      class="gallery__image"
-      src="${card.preview}"
-      data-source="${card.original}"
-      alt="${card.description}"
-    />
-  </a>
-</li>`
-    }).join('');
-    return markUp;
+function createItemsMarkup(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original.value}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
 }
+const onContainerClick = (e) => {
+  e.preventDefault();
 
-
-
-
-function openModalPhotoOnClick(event) {
-    event.preventDefault();
-
-    const currentImg = event.target.dataset.source;
+  if (e.target.classList.contains("gallery")) return;
+    const source = e.target.dataset.source;
     
-    if(!currentImg) {
-        return;
-    }
-    
-    const instance = basicLightbox.create(`
-    <img src="${currentImg}" width="800" height="600">
-`)
+  const instance = basicLightbox.create(`
+    <img src="${source}"width="800" height="600">`);
 
-instance.show()
-}
+  instance.show();
+};
 
-galleryEl.addEventListener('click', openModalPhotoOnClick);
+galleryContainerEl.addEventListener("click", onContainerClick);
