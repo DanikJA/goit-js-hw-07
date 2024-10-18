74cbb23178fc63fd7d01dda86ae0aaa47b68cc2b
Danik2023 <danik.kyrteu2@gmail.com>
2024-10-18 18:10:33 +0300

    ...

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,25 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
-  
 
 const galleryEl = document.querySelector('.gallery');
 
 const getEl = makeGalleryFn(galleryItems);
 galleryEl.insertAdjacentHTML('beforeend', getEl);
-////////////////////////////////////////////////////////////////////////////////
-function makeGalleryFn(cards){
+
+function makeGalleryFn(cards) {
     const markUp = cards.map(card => {
-        return `<li class="gallery__item">
-  <a class="gallery__link" href="${card.original}">
-    <img
-      class="gallery__image"
-      src="${card.preview}"
-      data-source="${card.original}"
-      alt="${card.description}"
-    />
-  </a>
-</li>`
+        return <li class="gallery__item">
+            <a class="gallery__link" href="${card.original}">
+                <img
+                    class="gallery__image"
+                    src="${card.preview}"
+                    data-source="${card.original}"
+                    alt="${card.description}"
+                />
+            </a>
+        </li>;
     }).join('');
     return markUp;
 }
@@ -28,32 +27,30 @@ function makeGalleryFn(cards){
 let instance;
 
 function openModalPhotoOnClick(event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  const currentImg = event.target.dataset.source;
+    const currentImg = event.target.dataset.source;
     
-  if (!currentImg) {
-    return;
-  }
-    
-  instance = basicLightbox.create(`
-    <img src="${currentImg}" width="800" height="600">
-
-  ,{  onClose: () => {
-       window.removeEventListener('keydown', closeOnClick);
+    if (!currentImg) {
+        return;
     }
-  }
-`);
-  instance.show();
-  window.addEventListener('keydown', closeOnClick);
+    
+    instance = basicLightbox.create(
+        <img src="${currentImg}" width="800" height="600">
+    , {
+        onClose: () => {
+            window.removeEventListener('keydown', closeOnClick);
+        }
+    });
 
-};
-  
-
-function closeOnClick(event){
-  if(event.code === 'Escape') {
-    instance.close();
-  }
+    instance.show();
+    window.addEventListener('keydown', closeOnClick);
 }
 
-galleryEl.addEventListener('click', openModalPhotoOnClick);     
+function closeOnClick(event) {
+    if (event.code === 'Escape') {
+        instance.close();
+    }
+}
+
+galleryEl.addEventListener('click', openModalPhotoOnClick);
