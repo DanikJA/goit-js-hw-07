5ce14f5ab233ead51bc5af39acc7e196d393d8c6
Danik2023 <danik.kyrteu2@gmail.com>
2024-10-19 20:00:23 +0300

    added function close window by pressing the Escape button

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,40 +4,88 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
   
 
+// const galleryEl = document.querySelector('.gallery');
+// const getEl = makeGalleryFn(galleryItems);
+// galleryEl.insertAdjacentHTML('beforeend', getEl);
+
+// function makeGalleryFn(cards){
+//     const markUp = cards.map(card => {
+//         return `<li class="gallery__item">
+//   <a class="gallery__link" href="${card.original}">
+//     <img
+//       class="gallery__image"
+//       src="${card.preview}"
+//       data-source="${card.original}"
+//       alt="${card.description}"
+//     />
+//   </a>
+// </li>`
+//     }).join('');
+//     return markUp;
+// }
+
+// function openModalPhotoOnClick(event) {
+//     event.preventDefault();
+
+//     const currentImg = event.target.dataset.source;
+    
+//     if(!currentImg) {
+//         return;
+//     }
+    
+//   const instance = basicLightbox.create(`
+//     <img src="${currentImg}" width="800" height="600">
+// `);
+  
+// instance.show()
+// }
+
+// galleryEl.addEventListener('click', openModalPhotoOnClick);   
+
 const galleryEl = document.querySelector('.gallery');
-const getEl = makeGalleryFn(galleryItems);
+const getEl = createGallery(galleryItems);
 galleryEl.insertAdjacentHTML('beforeend', getEl);
 
-function makeGalleryFn(cards){
-    const markUp = cards.map(card => {
-        return `<li class="gallery__item">
-  <a class="gallery__link" href="${card.original}">
+function createGallery(cards){
+  const markUp = cards.map(({original,preview,description}) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
-      src="${card.preview}"
-      data-source="${card.original}"
-      alt="${card.description}"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
 </li>`
-    }).join('');
-    return markUp;
+  }).join('')
+  return markUp;
 }
 
-function openModalPhotoOnClick(event) {
-    event.preventDefault();
 
-    const currentImg = event.target.dataset.source;
-    
-    if(!currentImg) {
-        return;
-    }
-    
+function openModalWindow(event){
+  event.preventDefault();
+
+  const currentImg = event.target.dataset.source;
+
+  if(!currentImg){
+    return;
+  }
+  
   const instance = basicLightbox.create(`
     <img src="${currentImg}" width="800" height="600">
-`);
-  
-instance.show()
+`)
+  instance.show();
+
+  const closeModal = (e) => {
+    if(e.key === 'Escape') {
+      instance.close();
+      document.removeEventListener('keydown',closeModal )
+   }
+  }
+  document.addEventListener('keydown', closeModal);
 }
 
-galleryEl.addEventListener('click', openModalPhotoOnClick);   
+
+
+galleryEl.addEventListener('click', openModalWindow);
